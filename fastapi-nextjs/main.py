497cc1ecdf5b912d6eca30764d54b0dbe6b35ca5
Peerasanti <peerasanti.s@kkumail.com>
2025-03-05 20:00:55 +0700

    connect FastAPI to mongodb atlas

--- fastapi-nextjs/main.py
+++ fastapi-nextjs/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from motor.motor_asyncio import AsyncIOMotorClient
+from contextlib import asynccontextmanager
+import urllib.parse
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+ENCODE_USERNAME = urllib.parse.quote_plus(os.getenv("MONGO_USERNAME"))
+ENCODE_PASSWORD = urllib.parse.quote_plus(os.getenv("MONGO_PASSWORD"))
+
+MONGO_URI = f"mongodb+srv://{ENCODE_USERNAME}:{ENCODE_PASSWORD}@rat-cluster.b3ctd.mongodb.net/?retryWrites=true&w=majority&appName=Rat-Cluster"
 
 app = FastAPI()
+
+client = None
+db = None
 
 app.add_middleware(
     CORSMiddleware,
@@ -11,7 +25,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global client, db
+    client = AsyncIOMotorClient(MONGO_URI)
+    db = client.get_database("rat-database")
+    print("Connected to MongoDB successfully!")
+    yield
+    client.close()
+    print("Disconnected from MongoDB.")
+
+app.router.lifespan_context = lifespan
+
 # for testing fastAPI
 @app.get("/")
 async def read_root():
     return {"message": "Hello from FastAPI"}
+
+# add favicon endpoint for avoid 404 wanring
+@app.get("/favicon.ico")
+async def favicon():
+    return {"message": "No favicon available"}
